7e5f4b10e74881c62029f178fac313af5844f27f
jungwonwoong <wjddnjsdnd@gmail.com>
2023-02-26 18:18:19 +0900

    영상제작용호출어플

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {io} from 'socket.io-client'
+const SERVER_ADD = 'http://192.168.0.13:3000/hallcall'
+const socket = io(SERVER_ADD, {
+  transports: ["websocket"],
+})
+socket.on("connect", () => {
+  console.log(socket.id)
+})
 export default function App() {
   return (
     <View style={styles.container}>
