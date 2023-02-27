908ede27cabeef11008b2867a057bc6c5da636c8
jungwonwoong <wjddnjsdnd@gmail.com>
2023-02-28 04:06:25 +0900

    first commit

--- App.js
+++ App.js
@@ -5,14 +5,25 @@ const SERVER_ADD = 'http://192.168.0.13:3000/hallcall'
 const socket = io(SERVER_ADD, {
   transports: ["websocket"],
 })
-socket.on("connect", () => {
-  console.log(socket.id)
-})
+// socket.on("connect", () => {
+//   console.log(socket.id)
+// })
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true} />
+      <View style={[styles.callbutton,{width:300,height:300,borderWidth:1,borderRadius:150,
+      backgroundColor:'#371100'}]}>
+        <View style={[styles.callbutton,{width:200,height:200,borderWidth:1,borderRadius:100,
+        backgroundColor:'#161616'}]}>
+          <View style={{position:'absolute',width:20,height:20,borderRadius:10,top:20,
+          backgroundColor:'white'}}></View>
+          <View style={[styles.callbutton,{width:100,height:100,borderWidth:2,borderRadius:50,
+          borderColor:'white'}]}>
+            <Text style={{fontSize:30,color:'white'}}>호출</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -24,4 +35,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  callbutton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
