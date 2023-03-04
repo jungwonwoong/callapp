1cd2606e7768e00089092a982117b8e0d78f96b4
jungwonwoong <wjddnjsdnd@gmail.com>
2023-03-04 23:30:24 +0900

    영상3편

---
+++ App.js
@@ -0,0 +1,55 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {io} from 'socket.io-client'
+const SERVER_ADD = 'http://172.30.1.38:3000/hallcall'
+const socket = io(SERVER_ADD, {
+  transports: ["websocket"],
+})
+// socket.on("connect", () => {
+//   console.log(socket.id)
+// })
+var btnOff = false
+export default function App() {
+  const [LEDc, setLEDc] = useState('white')
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden={true} />
+      <View style={[styles.callbutton,{width:300,height:300,borderWidth:2,borderRadius:150,
+      backgroundColor:'#371100'}]}>
+        <View style={[styles.callbutton,{width:200,height:200,borderWidth:2,borderRadius:100,
+        backgroundColor:'#161616'}]}>
+          <View style={{position:'absolute',width:20,height:20,borderRadius:10,top:20,
+          backgroundColor:LEDc}}></View>
+          <TouchableOpacity onPress={()=> {
+            btnOff = true
+            setLEDc('red');
+            setTimeout(()=>{
+              btnOff=false
+              setLEDc('white')
+            },3000)
+            socket.emit('sendcall', '호출신호보냄')
+          }}
+          disabled={btnOff}
+          style={[styles.callbutton,{width:100,height:100,borderWidth:2,borderRadius:50,
+          borderColor:'white'}]}>
+            <Text style={{fontSize:30,color:'white'}}>호출</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  callbutton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
